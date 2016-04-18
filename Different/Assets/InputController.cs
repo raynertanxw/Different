3500334369bb36b1df3510599484ab94f53bba53
Tan Xue Wen, Rayner <raynertanxw@me.com>
2016-04-18 15:03:14 +0800

    Implemented Pleb mouse controls

---
+++ Different/Assets/InputController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour
+{
+	private static InputController sInstance = null;
+	public static InputController Instance { get { return sInstance; } }
+
+	public Vector2 mouseWorldPos = Vector2.zero;
+
+	private void Awake()
+	{
+		if (sInstance == null)
+		{
+			sInstance = this;
+		}
+		else if (sInstance != this)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+
+	private void Update()
+	{
+		mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+
+	private void OnDestroy()
+	{
+		if (sInstance == this)
+			sInstance = null;
+	}
+}
---
+++ Different/Assets/Plebs/PlebController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Rigidbody2D))]
+public class PlebController : MonoBehaviour
+{
+	private Rigidbody2D thisRB;
+	private static float ForceMultiplier = 50.0f;
+
+	private void Awake()
+	{
+		thisRB = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		// Left click
+		if (Input.GetMouseButton(0))
+		{
+			Attract();
+		}
+
+		// Right click
+		else if (Input.GetMouseButton(1))
+		{
+			Repel();
+		}
+	}
+
+	private void Attract()
+	{
+		thisRB.AddForce(ForceToMFM(), ForceMode2D.Force);
+	}
+
+	private void Repel()
+	{
+		thisRB.AddForce(-ForceToMFM(), ForceMode2D.Force);
+	}
+
+	private Vector2 ForceToMFM()
+	{
+		Vector2 distVec = InputController.Instance.mouseWorldPos - (Vector2)transform.position;
+		float proximityForceMultiplier = 1.0f / distVec.sqrMagnitude;
+		if (proximityForceMultiplier > 10.0f)
+			proximityForceMultiplier = 10.0f;
+		distVec *= proximityForceMultiplier;
+		return distVec * ForceMultiplier;
+	}
+}
