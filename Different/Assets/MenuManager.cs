b744857ee6c605e02cc1cd763678b5b44dd28d8b
Petraller <clarencetay.2005@hotmail.com>
2016-04-18 17:50:10 +0800

    Difficulty and sliding
    - Added difficulty screen
    - Added transition effects

--- Different/Assets/MenuManager.cs
+++ Different/Assets/MenuManager.cs
@@ -1,16 +1,42 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using DaburuTools;
+using DaburuTools.Action;
 
 public class MenuManager : MonoBehaviour
 {
-    private void Awake()
-    {
+    public GameObject[] canvases;
 
-    }
-    
-    public void ChangeScene(string scene)
+    public void ChangeScene(string _scene)
     {
-        SceneManager.LoadScene(scene);
+        SceneManager.LoadScene(_scene);
+    }
+
+    public void ChangeScene(int _index)
+    {
+        SceneManager.LoadScene(_index);
+    }
+
+    public void SlideUp()
+    {
+        Slide(Vector2.up);
+    }
+
+    public void SlideDown()
+    {
+        Slide(Vector2.down);
+    }
+
+    private void Slide(Vector2 _direction)
+    {
+        _direction = new Vector2(Mathf.Round(Mathf.Clamp(_direction.x, -1, 1)), Mathf.Round(Mathf.Clamp(_direction.y, -1, 1)));
+        float width = 19.2f, height = 10.8f;
+        foreach (GameObject canvas in canvases)
+        {
+            //canvas.transform.localPosition += new Vector3(width * _direction.x, height * _direction.y, 0);
+            GraphLocalMoveByAction localMove = new GraphLocalMoveByAction(canvas.transform, Graph.SmoothStep, new Vector3(width * _direction.x, height * _direction.y, 0), 0.5f);
+            ActionHandler.RunAction(localMove);
+        }
     }
 }
