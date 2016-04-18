2bef55b45eca931730032021860d3a538b4ed9b9
Petraller <clarencetay.2005@hotmail.com>
2016-04-18 17:01:56 +0800

    Menu Scene
    - Simple menu screen with play button

---
+++ Different/Assets/MenuManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour
+{
+    private void Awake()
+    {
+
+    }
+    
+    public void ChangeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
