70e412e10b55e57abb712a908b0548035d812d85
Tan Xue Wen, Rayner <raynertanxw@me.com>
2016-04-18 16:13:55 +0800

    PlebControllers are now object pooled.
    Also made a make-shift GameManager

---
+++ Different/Assets/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+	private static GameManager sInstance = null;
+	public static GameManager Instance { get { return sInstance; } }
+
+	private void Awake()
+	{
+		if (sInstance == null)
+		{
+			sInstance = this;
+		}
+		else if (sInstance != this)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+
+	private void Start()
+	{
+		for (int i = 0; i < 30; i++)
+		{
+			PlebController.Spawn((Vector3) Random.insideUnitCircle);
+		}
+	}
+
+	private void OnDestroy()
+	{
+		if (sInstance == this)
+		{
+			sInstance = null;
+		}
+	}
+}
--- Different/Assets/Pleb_N_Blep/PlebController.cs
+++ Different/Assets/Pleb_N_Blep/PlebController.cs
@@ -1,19 +1,92 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-[RequireComponent (typeof(Rigidbody2D))]
 public class PlebController : MonoBehaviour
 {
-	private Rigidbody2D thisRB;
+	private static List<PlebController> PlebControllers = null;
 	private static float ForceMultiplier = 50.0f;
+	private static int NumPlebs { get { return PlebControllers.Count; } }
+
+	private Rigidbody2D thisRB;
+	private Collider2D thisCol2D;
+	private SpriteRenderer thisSpriteRen;
+	private bool mbEnabled;
 
 	private void Awake()
 	{
 		thisRB = gameObject.GetComponent<Rigidbody2D>();
+		thisCol2D = gameObject.GetComponent<Collider2D>();
+		thisSpriteRen = gameObject.GetComponent<SpriteRenderer>();
+
+		SetEnabled(false);
+
+		if (PlebControllers == null)
+		{
+			PlebControllers = new List<PlebController>();
+		}
+
+		PlebControllers.Add(this);
 	}
+
+	private void OnDestroy()
+	{
+		PlebControllers.Remove(this);
+		if (PlebControllers.Count == 0)
+		{
+			PlebControllers = null;
+		}
+	}
+
+	#region Pool Spawn Controls
+	public static PlebController Spawn(Vector3 _spawnPos)
+	{
+		for (int i = 0; i < PlebControllers.Count; i++)
+		{
+			if (PlebControllers[i].mbEnabled == false)
+			{
+				PlebController spawningPleb = PlebControllers[i];
+				spawningPleb.transform.position = _spawnPos;
+				spawningPleb.SetEnabled(true);
+
+				return spawningPleb;
+			}
+		}
+
+		// HOPE THIS NEVER HAPPENS. IF IT DOES, EXPAND POOL
+		Debug.LogWarning("Not enough Plebs, EXPAND POOL!!!");
+		return null;
+	}
+
+	public void ReturnToPool()
+	{
+		SetEnabled(false);
+	}
+
+	private void SetEnabled(bool _enabled)
+	{
+		if (_enabled)
+		{
+			thisRB.isKinematic = false;
+			thisCol2D.enabled = true;
+			thisSpriteRen.enabled = true;
+		}
+		else
+		{
+			thisRB.isKinematic = true;
+			thisCol2D.enabled = false;
+			thisSpriteRen.enabled = false;
+		}
+
+		mbEnabled = _enabled;
+	}
+	#endregion
 
 	private void Update()
 	{
+		if (!mbEnabled)
+			return;
+
 		// Left click Attracts
 		if (Input.GetMouseButton(0))
 		{
