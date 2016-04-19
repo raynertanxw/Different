c1ae373c6ac74ab975d154cc76ea697a556a3155
Tan Xue Wen, Rayner <raynertanxw@me.com>
2016-04-19 13:35:00 +0800

    Completed Medium Level

---
+++ Different/Assets/Levels/Level_Prop_MediumRotate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using DaburuTools.Action;
+
+public class Level_Prop_MediumRotate : MonoBehaviour
+{
+	public float mfPeriod = 5.0f;
+
+	void Awake()
+	{
+		RotateByAction2D rotByAction2D = new RotateByAction2D(this.transform, 360.0f, mfPeriod);
+		ActionHandler.RunAction(new ActionRepeatForever(rotByAction2D));
+	}
+}
