600d53d6d249c8fbe35775b6e8ef09da2b32e9ae
Tan Xue Wen, Rayner <raynertanxw@me.com>
2016-04-19 13:50:22 +0800

    Implemented Hard Level

---
+++ Different/Assets/Levels/Level_Prop_HardOrbit.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using DaburuTools.Action;
+using DaburuTools;
+
+public class Level_Prop_HardOrbit : MonoBehaviour
+{
+	public Transform orbitPivotTransform;
+	public float mfCycleDuration = 5.0f;
+
+	private void Awake()
+	{
+		OrbitAction2D orbit2D = new OrbitAction2D(this.transform, orbitPivotTransform, false, 1,
+			Graph.Linear, mfCycleDuration, true);
+		ActionHandler.RunAction(new ActionRepeatForever(orbit2D));
+	}
+}
